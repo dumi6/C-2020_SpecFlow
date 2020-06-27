ea0b96da6315274baf7377f7f2da3b4ce8baedaa
Dumi Sibanda <dumi.sibanda@virtual-college.co.uk>
2020-06-27 22:22:59 +0100

    New CustomerAccount test has been added by DS

---
+++ JuneProject/PageObjects/CustomerAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuneProject.PageObjects
+{
+    public class CustomerAccount
+    {
+        public CustomerAccount()
+        {
+            Console.WriteLine("The new customer class has been added");
+        }
+    }
+}
